5e0f391cc8aa2a0f72248bb0021373f756a269ca
Dilip <kumardilipale@gmail.com>
2020-09-10 18:44:41 +0545

    Revert "Revert "last file""
    This reverts commit 93c6ee2c8a8417811f17c419e15c8839f20698bf.

---
+++ Controllers/image.js
@@ -0,0 +1,35 @@
+const clarifai = require('clarifai');
+
+
+const app = new Clarifai.App({
+    apiKey: 'cf3411fbe0b04f3ba643da85311575cb'
+   });
+const handleApiCall=(req,res)=>{
+    app.models
+        .predict(Clarifai.FACE_DETECT_MODEL,req.body.input) 
+        .then(data=>{
+            res.json(data);
+        })
+        .catch(err=>res.status(400).json('unable to load api'))
+}
+   
+
+
+
+
+const handleImage=(req,res,db)=>{
+    const {id}= req.body;
+    db('users').where('id', '=', id)
+    .increment('entries',1)
+    .returning('entries')
+    .then(entries=>{
+        res.json(entries[0]);
+    })
+    .catch(eer=>res.status(400).json('unable to get entries'))
+  }
+  module.exports={
+      handleImage,
+      handleApiCall
+  }
+
+  
---
+++ server.js
@@ -0,0 +1,81 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+const bcrypt =require('bcrypt-nodejs');
+const cors=require('cors');
+const knex=require('knex');
+const image=require('./Controllers/image');
+const register=require('./Controllers/register');
+const profile=require('./Controllers/profile');
+const signIn=require('./Controllers/signin');
+
+
+
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : 'postgres123',
+      database : 'smart-brain'
+    }
+  });
+
+
+
+const app=express();
+app.use(bodyParser.json());
+app.use(cors())
+
+const database={
+    users:[
+        {
+            id:"123",
+            name:"Dilip",
+            email:"dkt@gmail.com",
+            password:'nara',
+            entries:0,
+            joined:new Date()
+
+        },
+        {
+            id:"124",
+            name:"Krishna",
+            email:"krishna@gmail.com",
+            password:'pokhara',
+            entries:0,
+            joined:new Date()
+
+        }
+
+    ]
+
+}
+app.get('/',(req,res)=>{res.send(database.users)})
+app.post('/signin',signIn.handleSignin(db,bcrypt))
+app.post('/register',(req,res)=>{register.handleRegister(req,res,db,bcrypt)})  
+app.get('/profile/:id',(req,res)=>{profile.handleProfileGet(req,res,db)})
+app.put('/image',(req,res)=>{image.handleImage(req,res,db)})
+app.post('/imageurl',(req,res)=>{image.handleApiCall(req,res)})
+
+
+
+
+    
+    //     let found=false;
+    // database.users.forEach(user=>{
+    //     if(user.id===id){
+    //         found=true;
+    //         user.entries++
+    //         return res.json(user.entries);
+    //     }
+    // })
+    // if(!found){
+    //     res.status(400).json('img not found');
+    // }
+
+
+app.listen(3000,(req,res)=>{
+    console.log("i am back");
+    
+});
