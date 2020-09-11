1738d60b0613c4dc8d63077d10e08ff1f9b71b46
Dilip <kumardilipale@gmail.com>
2020-09-11 16:30:28 +0545

    fix database

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const signIn=require('./Controllers/signin');
 const db = knex({
     client: 'pg',
     connection: {
-        connectionString:process.env.database_URL,
+        connectionString:process.env.DATABASE_URL,
         ssl:true,
     //   host : '127.0.0.1',
     //   user : 'postgres',
